5491f7d8c6de342c74d93a90fd6567e713ec75d0
entering-a-username <tamunacer@gmail.com>
2023-04-29 17:56:08 +0400

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,31 @@ module.exports = {
   content: ["index.html", "script.js"],
   theme: {
     fontFamily: {
-      'Tungsten': ['Tungsten-Bold', 'arial', 'georgia', 'sans-serif']
+      'Tungsten': ['Tungsten-Bold', 'arial', 'georgia', 'sans-serif'],
+      'footer-font': ['FF-Mark-W05', 'arial', 'sans-serif'],
     },
     letterSpacing: {
-      veryTight: '-.1em'
+      veryTight: '-.1em',
+      veryVeryTight: '-.15em',
+      wide: '.2em',
+    },
+    screens: {
+      'smsm': '100px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+      'sm-md': '674px',
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
     extend: {
       backgroundImage: {
