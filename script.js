06772e447a78f0ccc8db2f115282a50a816802b7
entering-a-username <tamunacer@gmail.com>
2023-04-30 14:27:16 +0400

    commit

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 (function() {
 
+    const hamburger = document.getElementById("hamburgerID");
+    const menu = document.getElementById("menuID");
+    const closeMenu = document.getElementById("closemenuID");
+    const deleteInput = document.getElementById("deleteinputID");
+    const input = document.getElementById("inputID");
+
+    const gameInfo = document.getElementById("gameinfoID");
+    const support = document.getElementById("supportID");
+    const ourSocials = document.getElementById("oursocialsID");
+    const gameInfoMenu = document.getElementById("gameinfomenuID");
+    const supportMenu = document.getElementById("supportmenuID");
+    const ourSocialsMenu = document.getElementById("oursocialsmenuID");
+
+
+
+    hamburger.addEventListener("click", function() {
+        menu.classList.remove("hidden");
+    })
+
+    closeMenu.addEventListener("click", () => {
+        menu.classList.add("hidden");
+    })
+
+    deleteInput.addEventListener("click", () => {
+        input.value = "";
+    })
+
+    gameInfo.addEventListener("click", () => {
+        gameInfoMenu.classList.toggle("hidden");
+    })
+
+    support.addEventListener("click", () => {
+        supportMenu.classList.toggle("hidden");
+    })
+
+    ourSocials.addEventListener("click", () => {
+        ourSocialsMenu.classList.toggle("hidden");
+    })
     
 
 })();
